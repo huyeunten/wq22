028476816b3ed3221bcb74541bedf1089aadfe4f
huyeunten <uyeuntenhale@seattleu.edu>
2022-01-11 09:21:23 -0800

    added tuple_sum, data hw complete up to 3

--- cpsc3400/0110_tuple_sum.py
+++ cpsc3400/0110_tuple_sum.py
@@ -1,2 +1,15 @@
-#def tuple_sum(l, t):
-#    for 
+def tuple_sum(pairs, threshold):
+    """" generate pairs that exceed a certain threshold
+    :param pairs: list of tuples of pairs
+    :param threshold: int
+    :return: pair if sum is larger than threshold
+    >>> [s for s in tuple_sum([(1, 2), (3, 4), (0, 7.3)], 6)]
+    [7, 7.3]
+    """
+    for p in pairs:
+        a, b = p
+        sum = a + b
+        if sum >+ threshold:
+            yield sum
+
+print(tuple_sum([(1, 2), (3, 4), (0, 7.3)], 6))
