7153ce9f3cd2bbb9decca5e701c567620eb1a77e
huyeunten <uyeuntenhale@seattleu.edu>
2022-01-15 13:12:48 -0800

    i'm freee most stressful experience of my life

--- cpsc3400/hw1/hw1.py
+++ cpsc3400/hw1/hw1.py
@@ -5,6 +5,8 @@ CPSC 3400
 Write more here
 """
 
+import sys
+
 # dictionary example: blue: (1, 2, 3)
 def process_file(file_name):
     """ Takes a file and creates a dictionary using colors as keys and
@@ -13,7 +15,11 @@ def process_file(file_name):
     :param file_name: The name of the file to read from.
     :return: A dictionary with color-tuple key-values.
     """
-    file = open(file_name, 'r')
+    try:
+        file = open(file_name, 'r')
+    except OSError:
+        print("Could not read file")
+        sys.exit(1)
     dict = {}
     for line in file:
         s = line.split()
@@ -50,7 +56,17 @@ def get_first_place_votes(dict, color):
         return 0
     return dict[color][0]
 
-#def create_favorite_color_list(dict):
+def create_favorite_color_list(dict):
+    """ Creates a list of colors sorted by the number of first place votes, 
+    second place votes, third place votes, and alphabetical order of keys.
+    Only colors with at least one first place vote are included in the list.
+    :param dict: Dictionary containing color names and tuples of votes
+    :return: A sorted list of colors
+    """
+    sorted_color = sorted(sorted(dict.items(), key = lambda x:x[0]), 
+        key=lambda x:(x[1][0], x[1][1], x[1][2]), reverse=True)
+    favorite_color_list = [color[0] for color in sorted_color if color[1][0] > 0]
+    return favorite_color_list
 
 def create_color_score_dict(dict):
     """ Creates a dictionary of colors and scores based on the number of times
@@ -73,13 +89,18 @@ def print_dictionary(dict):
         print(i, ": ", dict[i], sep="")
 
 if __name__ == "__main__":
-    dict = process_file("sample1.txt")
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print("Usage: hw1.py text_file.txt")
+        sys.exit(1)
+    dict = process_file(file_name)
     print_dictionary(dict)
     blue_first_votes = get_first_place_votes(dict, "blue")
     print(blue_first_votes)
     green_first_votes = get_first_place_votes(dict, "green")
     print(green_first_votes)
-    # create_favorite_color_list
-    # print ^^
+    favorite_color_list = create_favorite_color_list(dict)
+    print(favorite_color_list)
     cs_dict = create_color_score_dict(dict)
     print_dictionary(cs_dict)
