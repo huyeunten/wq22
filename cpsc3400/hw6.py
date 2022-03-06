1a803b426a7e63f6a010494f321e42615257be76
huyeunten <uyeuntenhale@seattleu.edu>
2022-03-06 09:23:13 -0800

    overthinking 😌

--- cpsc3400/hw6.py
+++ cpsc3400/hw6.py
@@ -1,12 +1,10 @@
-"""Template for answers to be submitted as Part 1 of HW6, CPSC 3400, Spring 2021
-ONLY change the string constants below and hand in this file after RENAMING it
-to hw6.py. You can use (and modify) hw6_test.py to test your submission. Do NOT
-hand in hw6_template.py or hw6_test.py.
+"""
+hw6.py by Haley Uyeunten
 """
 
 a = r'[0-46-9]*5[0-46-9]*5[0-46-9]*'
-b = r'([1-9]|1[0-2]):([0-5][0-9]) (PM|AM)'
-c = r'(([^0-9]\w*, )*([^0-9]\w*))*'
-d = r'Your regular expression here for problem 1.d.'  # FIXME
-d_sub = r'Your substitution string here for problem 1.d'  # FIXME
+b = r'([1-9]|1[0-2]):([0-5][0-9])\s(PM|AM)'
+c = r'(?:(?:[^0-9]\w+,\s)*[^0-9]\w+)*'
+d = r'(?:(?P<a>\w*)\s*<(?P<equal>=?)\s*(?P<b>\w*))+'
+d_sub = r'\g<b> >\g<equal> \g<a>'
 
